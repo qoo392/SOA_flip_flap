184552a03e57c8080cec038944273fc857807da2
qoo392 <qoo392@hotmail.com>
2015-10-03 19:51:24 +0800

    implement take-csv

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,6 +1,7 @@
 require_relative 'tsv_buddy'
 
 class FlipFlap
-  # Do NOT create an initialize method
+
+  #
 
 end
--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,5 +1,6 @@
 # Module that can be included (mixin) to create and parse TSV data
 module TsvBuddy
+  require 'csv'
   # @data should be a data structure that stores information
   #  from TSV or Yaml files. For example, it could be an Array of Hashes.
   attr_accessor :data
@@ -7,7 +8,13 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    tsv_table = CSV.parse(tsv, col_sep: "\t", headers: true)
+    @data = tsv_table.map do |row|
+      one_record = {}
+      row.each_with_index do |col, i|
+        one_record[tsv_table.headers[i]] = col
+      end
+    end
   end
 
   # to_tsv: converts @data into tsv string
